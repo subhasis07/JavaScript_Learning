d40388a5af3e80bd21af46e874730171fe082f9a
Subhasis Pal <palsubhasis100@gmail.com>
2024-05-26 23:42:49 +0530

    Adding game Logic

--- 23.BubbleGame_Proj/script.js
+++ 23.BubbleGame_Proj/script.js
@@ -9,7 +9,7 @@ function makeBubble() {
     document.querySelector("#panelBottom").innerHTML=clutter;
 }
 
-var timer=0;
+var timer=10;
 function runTimer() {
     var timeInt= setInterval(() => {
         if(timer>0){
@@ -17,22 +17,42 @@ function runTimer() {
             document.querySelector("#timeVal").textContent=timer;
         }else{
             clearInterval(timeInt);
+            document.querySelector("#panelBottom").innerHTML=
+            `<h1>Game Over; </h1>
+            <p></p>
+            <h1>Your Score= ${document.querySelector("#scoreVal").textContent}</h1>
+            `;
         }
     }, 1000);
 }
 
+var hitRN=0;
 function getNewHit() {
-    var rn=Math.floor(Math.random()*10);
-    document.querySelector("#hitVal").textContent=rn;
+    hitRN=Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent=hitRN;
 }
 
 var score=0;
-function updateScore() {
+function updateScorePositive() {
     score+=10;
     document.querySelector("#scoreVal").textContent=score;    
 }
 
+function updateScoreNegative() {
+    score-=5;
+    document.querySelector("#scoreVal").textContent=score;    
+}
+
+document.querySelector("#panelBottom").addEventListener("click", function (details) {
+    var clickedNum=Number(details.target.textContent);
+    if(clickedNum === hitRN){
+        updateScorePositive();
+        makeBubble();
+        getNewHit();
+    }else{
+        updateScoreNegative();
+    }
+})
 makeBubble();
 runTimer();
 getNewHit();
-updateScore();
