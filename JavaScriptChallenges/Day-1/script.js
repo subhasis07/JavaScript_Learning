ca02b586a163490d92bbd2fc034fa71b995d0838
Subhasis Pal <palsubhasis100@gmail.com>
2024-07-13 17:59:56 +0530

    JS_Prac_1

---
+++ JavaScriptChallenges/Day-1/script.js
@@ -0,0 +1,49 @@
+//
+
+var no=1;
+let str="Subhasis";
+
+console.log(no);
+console.log(str);
+
+//
+
+const isPossible= true;
+
+console.log(isPossible);
+
+//
+
+var details = {
+    username:"Subho",
+    Roll: 7
+}
+
+var apiFetch= [3.6, "abc", true, 55, 7];
+
+console.log(typeof(no));
+console.log(typeof(str));
+console.log(typeof(isPossible));
+console.log(typeof(details));
+console.log(typeof(apiFetch));
+
+//
+
+let curr= 5;
+console.log(curr);
+curr=7;
+console.log(curr);
+
+
+//
+
+const con= 5;
+console.log(con);
+// con=7;
+// console.log(curr); // Error
+
+//
+
+const userInp= prompt("Please Enter here");
+
+console.log(typeof(userInp)); //prompt() always returns a string
