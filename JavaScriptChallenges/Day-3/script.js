6c54aaf19ca6880b007100bf043038d949780b02
Subhasis Pal <palsubhasis100@gmail.com>
2024-07-16 20:43:21 +0530

    JS-Challenge_Day3

---
+++ JavaScriptChallenges/Day-3/script.js
@@ -0,0 +1,50 @@
+//
+// var input=prompt("Enter a no: ");
+
+// if(input>0){
+//     console.log("It's a +ve no");
+// }else if(input<0){
+//     console.log("-ve no");
+// }else{
+//     console.log("zero");
+// }
+
+
+//
+
+// var input =prompt("Enter your age: ");
+// if(input>=18){
+//     console.log("Voting Eligible");
+// }else{
+//     console.log("Not eligible");
+// }
+
+//
+
+switch (new Date().getDay()) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:3
+        break;
+}
+
+//
