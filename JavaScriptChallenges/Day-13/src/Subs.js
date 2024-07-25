f4f548a43e8abc0d2adf9b29f59e7aab7613039f
Subhasis Pal <palsubhasis100@gmail.com>
2024-07-25 22:38:50 +0530

    JS challenge:Day-13

---
+++ JavaScriptChallenges/Day-13/src/Subs.js
@@ -0,0 +1,5 @@
+function Sub(a,b) {
+    return a-b;
+}
+
+export default Sub;
---
+++ JavaScriptChallenges/Day-13/src/script.js
@@ -0,0 +1,4 @@
+import {add} from './Addition.js'; //named ecport
+import Subs from './Subs.js'; //default export
+add(5,9);
+console.log(Subs(9,5));
